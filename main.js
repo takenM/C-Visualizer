b8d9b17e9c18288bd601895397272a4736da7b46
takenM <fukuyamasya1998-mdx@yahoo.co.jp>
2015-12-21 16:15:51 +0900

    やり方帰る

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ editor.setFontSize(14);
 editor.getSession().setMode("ace/mode/c_cpp");
 editor.getSession().setUseWrapMode(true);
 editor.getSession().setTabSize(4);
-editor.setValue("int a;int b;");
+editor.setValue("int a, b;\nint c;");
 var debbug = document.getElementById("debbug");
 
 
@@ -14,13 +14,21 @@ function main(){
 
     code = editor.getValue().split(";");
     console.log(code);
-
+    /**************:お前ちょっと寝てろ！！（ほかのやり方でやってみる）**************
     for(var i = 0; i < code.length - 1; i++){
+        //変数宣言の場合
         if( /int /.test(code[i]) ) {
             code[i] = code[i].replace(/int /, "");
+            code[i] = code[i].replace(/\s/,"");
+
             //console.log(code[i]);
             variables.push({label: code[i], value: null, type: "int", array: false});
             console.log(variables);
         }
+        //変数宣言の場合
     }
+    *****************************************************************************/
+
+    
+
 }
