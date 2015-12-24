c39669ec27e98b84925e7ba2e60090261ee2789a
takenM <fukuyamasya1998-mdx@yahoo.co.jp>
2015-12-24 17:43:43 +0900

    ちょっと付け足した

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ editor.setFontSize(14);
 editor.getSession().setMode("ace/mode/c_cpp");
 editor.getSession().setUseWrapMode(true);
 editor.getSession().setTabSize(4);
-editor.setValue("int a, b;\nint c;");
+editor.setValue(" int a, b;\nint c;");
 var debbug = document.getElementById("debbug");
 
 
@@ -14,6 +14,9 @@ function main(){
     var tempCode = new Array;
 
     code = editor.getValue().split(";");
+    //空要素削除
+    for(var j = 0; j < code.length; j++)
+        if(code[j] === '') code.splice(j,1);
     console.log(code);
     /**************:お前ちょっと寝てろ！！（ほかのやり方でやってみる）**************
     for(var i = 0; i < code.length - 1; i++){
@@ -29,9 +32,15 @@ function main(){
         //変数宣言の場合
     }
     *****************************************************************************/
+    //一週目！一番大きなループ 一文に区切る
     for(var i = 0; i < code.length; i++){
         tempCode = code[i].replace(/\n/, '');
         tempCode = tempCode.split(/\s|,/);
+        //空要素削除
+        for(var j = 0; j < tempCode.length; j++)
+            if(tempCode[j] === '') tempCode.splice(j,1);
+        //区切った文を処理していく
+
         console.log(tempCode);
     }
 
